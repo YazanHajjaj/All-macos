8cd861b71ad2dfde68b7a00f5757cac246a890c2
yazanhajjaj <yazanfr.hajjaj@gmail.com>
2024-02-16 03:08:03 +0300

    Revert "Files"
    This reverts commit 90c0af90392c5b735286e257700d851443fa53bc.

--- Labg/main.py
+++
@@ -1,7 +0,0 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n-1) +fibonacci(n -2)
-result = fibonacci(5)
-print("Fiabonacci sequence at index 5:", result)
